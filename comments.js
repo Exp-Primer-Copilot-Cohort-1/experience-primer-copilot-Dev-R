cf15064e80e5fba30c12fb3d023d61a9e879534d
R <83784102+Dev-R@users.noreply.github.com>
2023-12-28 17:29:51 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// create server
+const app = express();
+
+// configure server
+app.use(bodyParser.json());
+app.use(cors());
+
+// create route
+app.get('/comments', (req, res) => {
+    res.send('Hello world');
+});
+
+// start server
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
